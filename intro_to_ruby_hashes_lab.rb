4e4f935033a9e800c95f2eafc1584d96db9c7aba
Jenny Chu <jchudesign0930@gmail.com>
2019-02-20 05:21:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads => {:pieces => 4,
-	  {:name => {},{:rent_in_dollards => {}
-	}
+	  :railroads=> {
+	    :pieces =>4,:name=> {},:rent_indollars=>{}
+	  }
 	
 end
 
